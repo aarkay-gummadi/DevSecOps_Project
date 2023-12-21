9a471573318927c71c115582ba2a5d86cf9e38cb
qtaarkayapril23 <aarkay.gummadi@gmail.com>
2023-12-21 14:28:52 +0530

    Added the new changes in the project

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -1,12 +1,77 @@
----
-apiVersion: eksctl.io/v1alpha5
-kind: ClusterConfig
-metadata:
-  name: spot-cluster
-  region: us-west-2
-managedNodeGroups:
-# `instanceTypes` defaults to [`m5.large`]
-  - name: spot-1
-    spot: true
-    ssh:
-      publicKeyName: OregonEswar #replace with your public key pair
+terraform {
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "fordevsecops1"
+    key            = "workshopdec23.tfstate"
+    region         = "us-west-2"  # Specify the AWS region where your S3 bucket is located
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"
+  }
+}
+
+resource "aws_eks_cluster" "example" {
+  name     = "example-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.private[*].id
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster]
+}
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "example-eks-cluster"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+resource "aws_subnet" "private" {
+  count = 2
+  
+  cidr_block = "10.0.${count.index + 1}.0/24"
+}
+
+output "kubeconfig" {
+  value = aws_eks_cluster.example.kubeconfig[*].raw_config
+  sensitive = true
+}
+
+resource "null_resource" "get_kube_config" {
+  depends_on = [aws_eks_cluster.example]
+
+  triggers = {
+    cluster_id = aws_eks_cluster.example.id
+  }
+
+  provisioner "local-exec" {
+    command = "aws eks --region us-east-1 update-kubeconfig --name ${aws_eks_cluster.example.name}"
+  }
+}
