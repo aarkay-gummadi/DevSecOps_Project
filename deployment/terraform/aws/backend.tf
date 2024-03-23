e292fa5dbfc95f2ff7d279eb3ab4d1f19d91eaa8
qtaarkayapril23 <aarkay.gummadi@gmail.com>
2024-03-23 08:36:41 +0530

    Added the region to the bucketname

--- deployment/terraform/aws/backend.tf
+++ deployment/terraform/aws/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "waytos3"
+    bucket = "oregonbucketfordevsecops"
     key    = "eks/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-west-2"
   }
 }
