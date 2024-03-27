bff78114af5dee923c4d9174eec5f0774a51908c
qtaarkayapril23 <aarkay.gummadi@gmail.com>
2024-03-27 19:57:01 +0530

    Added the readme file

--- deployment/terraform/aws/backend.tf
+++ deployment/terraform/aws/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "oregonbucketfordevsecops"
+    bucket = "southbucketforaarkay"
     key    = "eks/terraform.tfstate"
-    region = "us-west-2"
+    region = "ap-south-1"
   }
 }
--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -1,67 +1,66 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
+provider "aws" {
+  region = "us-west-2"  # Specify your desired AWS region
+}
+
+# Create a VPC for EKS
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create subnets in different availability zones
+resource "aws_subnet" "eks_subnet" {
+  count = 3
+
+  cidr_block = "10.0.${count.index + 1}.0/24"
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+# Create IAM role for EKS cluster
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com",
+      },
+    }],
+  })
+}
+
+# Attach AmazonEKSClusterPolicy to the IAM role
+resource "aws_iam_role_policy_attachment" "eks_cluster" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+# Create EKS cluster
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "example-eks-cluster"
+  subnets         = aws_subnet.eks_subnet[*].id
+  vpc_id          = aws_vpc.eks_vpc.id
+  cluster_version = "1.21"  # Specify the desired Kubernetes version
+
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 1
+      max_capacity     = 2
+      min_capacity     = 1
+
+      key_name = "OregonEswar"  # Specify your SSH key name
+
+      instance_type = "t2.medium"  # Specify the desired instance type
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
   }
 }
 
-provider "azurerm" {
-  features {}
-}
-
-terraform {
-  backend "azurerm" {
-    storage_account_name = "idontknowlt"
-    container_name       = "tflock"
-    key                  = "workshopdec23.tfstate"
-    resource_group_name = "DefaultResourceGroup-EUS"
-  }
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
-}
-
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "exampleaks1"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    Environment = "Production"
-  }
-}
-
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+# Output kubeconfig for local usage
+output "kubeconfig" {
+  value     = module.eks.kubeconfig
   sensitive = true
-}
-
-
-resource "null_resource" "get_kube_config" {
-  triggers = {
-    cluster_id = azurerm_kubernetes_cluster.example.id
-  }
-
-  provisioner "local-exec" {
-    command = "az aks get-credentials --resource-group ${azurerm_resource_group.example.name} --name ${azurerm_kubernetes_cluster.example.name} --overwrite-existing"
-  }
 }
